3f182f03837a112199ea2dfa746f8ea9f47f3744
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2020-12-07 13:24:06 -0500

    Done.

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -12,7 +12,24 @@ class GetPrograms
     response.body
   end
 
+  def program_school
+    programs = JSON.parse(self.get_programs)
+    programs.collect {|program| program["agency"]}
+  end
+
+  def programs_by_age_group
+    programs = JSON.parse(self.get_programs)
+    programs.collect do |program|
+      "#{program["agency"]} - #{program["borough_community"]} - #{program["grade_level_age_group"]}"
+    end
+  end
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+# programs = GetPrograms.new.get_programs
+# puts programs
+
+# programs = GetPrograms.new
+# puts programs.program_school.uniq
+
+programs = GetPrograms.new
+puts programs.programs_by_age_group
